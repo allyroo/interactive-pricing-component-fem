9be691c8399928d537f89075d509985ffc5be3d5
allyroo <allyse.arucan@gmail.com>
2022-02-01 23:27:59 -0700

    slider

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+let slider = document.getElementById("priceSlider");
+let valueOutput = document.getElementById("valueOutput");
+let pageViewsOutput = document.getElementById("pageViewsOutput");
+
+let pageViewOptions = ["10k", "50k", "100k", "500k", "1M"];
+let priceOptions = [8, 12, 16, 24, 36];
+
+function updateOutput() {
+  const optionsIndex = parseInt(slider.value, 10);
+  pageViewsOutput.textContent = pageViewOptions[optionsIndex];
+  valueOutput.textContent = priceOptions[optionsIndex];
+}
+
+updateOutput();
+
+slider.addEventListener("input", updateOutput);
