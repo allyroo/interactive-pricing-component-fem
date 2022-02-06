aa2413d59a60657cbbee3fd7253ec1a5ac50739c
allyroo <allyse.arucan@gmail.com>
2022-02-05 23:02:28 -0700

    pushing code before i forget

--- index.js
+++ index.js
@@ -3,31 +3,20 @@ let valueOutput = document.getElementById("valueOutput");
 let pageViewsOutput = document.getElementById("pageViewsOutput");
 let isYearly = document.getElementById("toggle");
 
-let pageViewOptions = ["10k", "50k", "100k", "500k", "1M"];
+let pageViewOptions = ["10K", "50K", "100K", "500K", "1M"];
 let priceOptions = [8, 12, 16, 24, 36];
 
 function updateOutput() {
   const optionsIndex = parseInt(slider.value, 10);
+  const price = priceOptions[optionsIndex];
   pageViewsOutput.textContent = pageViewOptions[optionsIndex];
-  valueOutput.textContent = priceOptions[optionsIndex];
+  valueOutput.textContent = isYearly.checked ? price * 0.75 : price;
 }
 
 updateOutput();
 
-function toggleDiscount() {
-  isYearly.checked = !isYearly.checked;
-  if (isYearly.checked) {
-    priceOptions = priceOptions.map((x) => x * 0.75);
-    updateOutput()
-    console.log(isYearly.checked);
-  } else {
-    console.log("False");
-  }
-}
-
-
 slider.addEventListener("input", updateOutput);
-isYearly.addEventListener("click", toggleDiscount);
+isYearly.addEventListener("click", updateOutput);
 // console.log(toggle.checked);
 // function toggle() {
 //   let isMonthly = false
