7281568288ae03b7b47e38eaf17dec36c1917b70
Mike Chen <1325549+mhchen@users.noreply.github.com>
2022-02-11 09:49:49 -0800

    Live code review ep 1

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ let pageViewsOutput = document.getElementById("pageViewsOutput");
 let isYearly = document.getElementById("toggle");
 
 let pageViewOptions = ["10K", "50K", "100K", "500K", "1M"];
-let priceOptions = [8, 12, 16, 24, 36];
+let priceOptions = [6, 12, 16, 24, 36];
 
 function updateOutput() {
   const optionsIndex = parseInt(slider.value, 10);
   const price = priceOptions[optionsIndex];
   pageViewsOutput.textContent = pageViewOptions[optionsIndex];
-  valueOutput.textContent = isYearly.checked ? price * 0.75 : price;
+  const finalPrice = isYearly.checked ? price * 0.75 : price;
+  valueOutput.textContent = finalPrice.toLocaleString(undefined, {
+    style: 'currency',
+    currency: 'USD',
+  });
 }
 
 updateOutput();
